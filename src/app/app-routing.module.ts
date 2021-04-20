7b4bdef1c5a814a1a63a7c6a921ba6f254ea0bee
matteobavecchi <matteo.bavecchi@stud.unifi.it>
2021-04-20 16:03:24 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetail } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetail },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 import { Hero } from './hero';
-import { HEROES } from "../mock-heroes"
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +10,16 @@ import { HEROES } from "../mock-heroes"
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  selectedHero?: Hero;
-  constructor() { }
+  heroes!: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
-
 }
