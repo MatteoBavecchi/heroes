c06c91572d144797de34a73729f53e79c0dd4212
matteobavecchi <matteo.bavecchi@stud.unifi.it>
2021-04-20 17:06:21 +0200

    Added HTTP client

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -32,4 +32,9 @@ export class HeroDetail implements OnInit {
   goBack(): void {
     this.location.back();
   }
+  
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
